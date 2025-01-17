9795e76c85fbe2e452a3d186ab0e15c6f914594c
JaiJai <wm.rd01@gmail.com>
2025-01-17 16:05:01 +0800

    更新自適應

--- index.743ae.js
+++ index.743ae.js
@@ -1,4 +1,4 @@
-System.register(["./application.6fe9b.js"], function (_export, _context) {
+System.register(["./application.3e48f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
